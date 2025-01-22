11fbaa409a57bbcee83e8bb49303acb5c97ff530
habiburrahman30 <hmhabib999@gmail.com>
2025-01-22 10:23:12 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 import 'src/app.dart';
-import 'src/services/back_services.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Permission.notification.isDenied.then((value) {
-    if (value) {
-      Permission.notification.request();
-    }
-  });
-  await initializeService();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Permission.notification.isDenied.then((value) {
+  //   if (value) {
+  //     Permission.notification.request();
+  //   }
+  // });
+  // await initializeService();
   runApp(App());
 }
